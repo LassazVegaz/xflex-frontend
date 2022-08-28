4354811ea95591eac071e4c5cf72146af896081f
LassazVegaz <lasindunuwanga@gmail.com>
2022-08-28 15:37:44 +0530

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,34 @@
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+
 const Header = () => {
-	return <div>Hello World</div>;
+	return (
+		<Box flexGrow={1}>
+			<AppBar>
+				<Toolbar
+					sx={{
+						width: "100%",
+					}}
+				>
+					<Typography
+						variant="h5"
+						fontWeight={800}
+						component="div"
+						sx={{
+							flexGrow: 1,
+						}}
+					>
+						XFLEX CLOTHING STORE
+					</Typography>
+
+					<Box>
+						<Button variant="secondary">Shop</Button>
+						<Button variant="secondary">History</Button>
+						<Button variant="secondary">Login</Button>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 };
 
 export default Header;
