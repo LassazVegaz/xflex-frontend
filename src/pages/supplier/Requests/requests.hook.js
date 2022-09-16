02cb8bbbc4fe28e511211a1f7b8f6fa98d9b1cce
LassazVegaz <lasindunuwanga@gmail.com>
2022-09-16 20:34:19 +0530

    use reqs hook

--- src/pages/supplier/Requests/requests.hook.js
+++ src/pages/supplier/Requests/requests.hook.js
@@ -1,26 +1,35 @@
 import React from "react";
+import { NotificationManager } from "react-notifications";
+import { useParams } from "react-router-dom";
 import { supplierRequestApi } from "../../../utils/supplier-requests.api";
 
+const initialState = {
+	pending: [],
+	received: [],
+	canclled: [],
+};
+
 export const useRequests = () => {
-	const [requests, setRequests] = React.useState([]);
+	const { id } = useParams();
+	const [requests, setRequests] = React.useState(initialState);
 	const [isLoading, setIsLoading] = React.useState(false);
-	const [error, setError] = React.useState(null);
 
 	const getRequests = React.useCallback(async () => {
 		try {
 			setIsLoading(true);
-			const requests = await supplierRequestApi.getRequests();
+			const requests = await supplierRequestApi.getRequests(id);
 			setRequests(requests);
 		} catch (error) {
-			setError(error);
+			NotificationManager.error("Getting supplier requests data failed");
+			setRequests(initialState);
 		} finally {
 			setIsLoading(false);
 		}
-	}, []);
+	}, [id]);
 
 	React.useEffect(() => {
 		getRequests();
 	}, [getRequests]);
 
-	return { requests, isLoading, error };
+	return { requests, isLoading };
 };
