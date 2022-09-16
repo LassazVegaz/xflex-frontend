28753baa3a5599e91b77fb01bfe86738f8182092
LassazVegaz <lasindunuwanga@gmail.com>
2022-09-16 20:34:52 +0530

    integrating into req page

--- src/pages/supplier/Requests/RequestsPage.jsx
+++ src/pages/supplier/Requests/RequestsPage.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import { Helmet } from "react-helmet";
 import { PageContainer } from "../../../components/PageContainer/PageContainer";
+import SmallLoader from "../../../components/SmallLoader/SmallLoader";
+import REQUESTS_STATUESES from "../../../constants/sup-reqs-statuses";
+import RequestsList from "./components/RequestsList";
+import RequestsTabs from "./components/RequestsTabs";
+import { useRequests } from "./requests.hook";
 
 const RequestsPage = () => {
+	const { requests, isLoading } = useRequests();
+	const [tabIndex, setTabIndex] = useState(REQUESTS_STATUESES.PENDING);
+
 	return (
-		<PageContainer>
+		<>
 			<Helmet>
 				<title>Supplier Requests</title>
 			</Helmet>
-		</PageContainer>
+
+			<RequestsTabs tab={tabIndex} onChange={setTabIndex} />
+
+			<PageContainer>
+				{isLoading ? (
+					<SmallLoader minHeight="30vh" />
+				) : (
+					<RequestsList requests={requests[tabIndex] ?? []} />
+				)}
+			</PageContainer>
+		</>
 	);
 };
 
