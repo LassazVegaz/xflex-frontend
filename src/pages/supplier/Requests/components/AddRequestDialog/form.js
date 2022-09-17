c0fe95da65ed987b1c84f7a7f5145afc7370a040
LassazVegaz <lasindunuwanga@gmail.com>
2022-09-17 22:54:35 +0530

    req form

---
+++ src/pages/supplier/Requests/components/AddRequestDialog/form.js
@@ -0,0 +1,39 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+	items: [
+		{
+			__id: 0,
+			code: "",
+			amount: "",
+		},
+	],
+};
+
+const validationSchema = Yup.object().shape({
+	items: Yup.array().of(
+		Yup.object().shape({
+			code: Yup.string().required("Required"),
+			amount: Yup.number()
+				.required("Required")
+				.positive("Amount must be positive")
+				.integer("Amount must be a number")
+				.typeError("Amount must be a number"),
+		})
+	),
+});
+
+const useForm = () => {
+	const form = useFormik({
+		initialValues,
+		validationSchema,
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
+
+	return form;
+};
+
+export default useForm;
