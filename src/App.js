6beb64d1651da563d28b41146b044e40c0350947
Ariyarathna H.M.M.M <it20120634@my.sliit.lk>
2022-10-10 22:52:03 +0530

    started combining

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NotificationContainer } from "react-notifications";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header.old";
+import Footer from "./components/Footer/Footer.old";
 import MainPages from "./pages/Pages";
 import { ScreenLoader } from "./components/ScreenLoader/ScreenLoader";
 
